3c68f21e79bf13a2ee87ce13fb78e8c0909ba484
Abenezer <abenezera50@gmail.com>
2022-07-03 19:40:35 +0300

    Added

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -22,31 +21,4 @@ putchar(k);
 }
 putchar('\n');
 return (0);
-=======
- * main - main block
- * Description:  prints the alphabet in lowercase,
- * and then in uppercase, followed by a new line.
- * Return: 0
- */
-int main(void)
-{
-	char c = 97;
-
-	while (c <= 122)
-	{
-		putchar(c);
-		c++;
-	}
-
-	c = 65;
-
-	while (c <= 90)
-	{
-		putchar(c);
-		c++;
-	}
-
-	putchar('\n');
-	return (0);
->>>>>>> refs/remotes/origin/master
 }
