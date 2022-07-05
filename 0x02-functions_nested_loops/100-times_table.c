98487b6b689c685ff04a4ac0a68e0084e7e4a85d
abenezerapy <106971846+abenezerapy@users.noreply.github.com>
2022-07-05 22:15:32 +0300

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,48 @@
 #include "main.h"
-#include "stdio.h"
-
 /**
- * print_times_table - prints time table of n
- *
- * @n: takes number input
-*/
-
+ * print_times_table - prints the times table
+ * @n: takes in an integer
+ */
 void print_times_table(int n)
 {
-	int prod, mult, num;
+	int i, j, k;
 
-	if (n <= 15 && n >= 0)
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i <= n; i++)
 	{
-		for (num = 0; num <= n; ++num)
+		for (j = 0; j <= n; j++)
 		{
-			_putchar(48);
-			for (mult = 1; mult <= n; ++mult)
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			else if (k <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod <= 99)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
-				} else if (prod <= 99 && prod >= 10)
-					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 			}
-			_putchar('\n');
+			else if (k >= 10 && k < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(k / 10 % 10 + '0');
+				_putchar(k % 10 + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
