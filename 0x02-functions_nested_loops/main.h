327a60143049851e9acc50b78c6f9cb242ce66f8
abenezerapy <106971846+abenezerapy@users.noreply.github.com>
2022-07-05 19:21:39 +0300

    updating main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for all functions.
+ */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -7,9 +11,9 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int n);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int n1, int n2);
+int add(int, int);
 void print_to_98(int n);
 #endif
