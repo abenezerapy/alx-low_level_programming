d9df85abd5896dae0e8bdc46cbc610eb6b388b4e
abenezerapy <106971846+abenezerapy@users.noreply.github.com>
2022-07-05 19:29:32 +0300

    updating putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
- * main - Entry point
- *
- * Return: nothing, return void
- */
-
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+	char ermias[] = "_putchar";
 
-    return (0);
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
