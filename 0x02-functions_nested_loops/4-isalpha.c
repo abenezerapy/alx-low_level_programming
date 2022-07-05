a1d3bb8c56e74260978ee94981e0bc3c06af7d70
abenezerapy <106971846+abenezerapy@users.noreply.github.com>
2022-07-05 20:08:55 +0300

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * _isalpha - checks for alphabetic character
- * @c: takes in a character
- * Return: 1 if letter, lowercase, uppercase; 0 for otherwise
+ *_isalpha - checks for a alphabetic character using a function
+ *@c: letter being tested
+ * Return: Always 0 succesS
  */
+
 int _isalpha(int c)
 {
-    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
